9f40f355203195fa1463d714f59141c48b9a415e
Douglas Carlini <douglascarlini@gmail.com>
2019-12-09 23:31:05 -0300

    Update

--- game.js
+++ game.js
@@ -17,7 +17,7 @@ class Game {
         Game.cnv.width = Game.w;
         Game.last = new Date();
         Game.vel = 1 / 10;
-        Game.glow = true;
+        Game.glow = false;
         Game.items = {};
         Game.fps = 60;
         Game.uid = 0;
